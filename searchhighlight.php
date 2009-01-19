e805e45fa85ea33011455c3eaf14bbbaafda375b
Ansgar Bockstiegel <ansgar.bockstiegel@uos.de>
2009-01-19 11:31:19 +0100

    Initial commit

---
+++ searchhighlight.php
@@ -0,0 +1,30 @@
+<?php
+
+//define the style for search-terms
+$HTMLStylesFmt['highlight']=".highlight { background-color: #9999FF }";
+//get the referrer and separate the arguments
+$query=explode('&', parse_url($_SERVER['HTTP_REFERER'],PHP_URL_QUERY));
+//initialisation
+$terms=array();
+//check arguments if they are search-terms
+foreach($query as $part){
+    //separate argument in key-value-pairs
+    list($key, $value)=explode('=',$part);
+    //check if current argument is a search-term
+    if ($key=="q"){
+        //separate words and save them as search-terms
+        $terms=explode('+',$value);
+    }
+}
+//generate dynamic markup for each word the user was searching for
+foreach($terms as $term){
+    Markup("hl".$term, "<_end", "/(<[^>]*>|".$term.")/ie", "highlight('$1')");
+}
+
+function highlight($input){
+    if ($input[0]=="<"){
+        return $input;
+    }else{
+        return "<span class='highlight'>".$input."</span>";
+    }
+}
